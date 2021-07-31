8756230efa5c9c48140515a2bd043201bb4fc6e0
togo-mentor <uint2435@gmail.com>
2021-08-01 07:07:27 +0900

    error messages

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ApplicationRecord
   belongs_to :genre
+  with_options presence: true do
+    validates :name
+    validates :explanation
+    validates :deadline_date
+    validates :status
+  end
 end
